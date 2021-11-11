55a4514737bb1250497438092d934f3131e0ba7f
Javier Bahamón Parrado <xjavierb@gmail.com>
2021-11-10 22:37:09 -0500

    clase 28 octi 2021

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*
+ejemplo de 3 micro servicios de prueba
+*/
+
 // basado en apuntes.de/nodejs-desarrollo-web/body-parser/#gsc.tab=0
 // como instalar un body parser
 const { response } = require("express");
@@ -6,7 +10,8 @@ const server = express();   // servidor, de aqui sale para abrir el servidor y i
 
 const empleado = {
     "primerNombre": "juan",
-    "segundoNombre": "Alberto"
+    "segundoNombre": "Alberto",
+    "numeroDocumento": "123456"
 };
 
 //cuando se llame va a entrar por este lado, recibe una funcion
@@ -14,12 +19,31 @@ server.get("/", function(request, response){
     response.send("Servicio raiz....");
 });
 
-
 // segundo servicio
     // entrega object a nivel del JS, se vera en pantalla
 server.get('/empleado', function(request, response){
     response.send(empleado);
 });
+});
+
+// prueba 3
+server.get("/prueba", function(request, response){
+    response.send(server);
+});
+
+// prueba 4
+server.get("/empleado/:id", function(request, response){
+    const idObj = request.params.id;
+    if(empleado.numeroDocumento === idObj){
+        response.send(empleado);
+    }else{
+        response.send("no exist empleado");
+    }
+});
+
+// prueba 5
+
+
 
 // apertura del puerto para escuchar peticiones entrantes mediante el uso de una expresion lamda
 server.listen(
